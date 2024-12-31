ed94947ada166b3d20e6964b5a948fe9bc955ccb
MuhammadHammadArshad123 <hammadarshad@satjapan.co>
2024-12-31 17:46:51 +0500

    Design Sell with sat

---
+++ resources/views/static_pages/partial/sellWithSat.blade.php
@@ -0,0 +1,67 @@
+<section class="why-title-desktop">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <img src="{{asset('assets/images/staticPagesAssets/images/whysell-bannar.webp')}}" class="img-fluid" height="274" width="1266" loading="lazy" alt="sat bannar">
+              </div>
+
+              <div class="col-12 py-5">
+                <h1 class="why-title mw-100">Why Join Hands With SAT JAPAN?</h1>
+                <p class="why-title-para mw-100">We lead the way in connecting buyers with their dream used cars worldwide; lending you our trusted expertise, we make car purchasing simpler. We have empowered millions of customers with our reliable and transparent cross-border services. Join our thriving community of certified Japanese car owners and experience unparalleled convenience.</p>
+              </div>
+
+
+             <div class="row g-3">
+                <div class="col-md-6 order-md-1 order-sm-2 order-2  col-sm-12 d-flex flex-column justify-content-center">
+                    <h2 class="why-title w-100">Our Services Reach More Than 200 Countries</h2>
+                    <p class="why-title-para">We operate globally and have an established presence in more than 200 countries. We have gained a global network of customers through online retailing due to our efforts to make car purchasing accessible to all. </p>
+                  </div>
+                  <div class="col-md-6 col-sm-12 order-md-2 order-sm-1 order-1 text-end img-div">
+                    <img src="{{asset('assets/images/staticPagesAssets/images/whysell-1.webp')}}" width="500" height="331" class="sellimage img-fluid" loading="lazy" alt="sat img">
+                  </div>
+             </div>
+
+
+              <div class="row g-3">
+                <div class="col-md-6 col-sm-12  d-flex flex-column justify-content-center img-div">
+                  <img src="{{asset('assets/images/staticPagesAssets/images/whysell-2.webp')}}" width="500" height="331" class="sellimage img-fluid" loading="lazy" alt="sat img">
+                </div>
+                <div class="col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-end">
+                  <h2 class="why-title w-100">Over One Million Cars Sold — And Counting!</h2>
+                  <p class="why-title-para">Based in Iwaki, Fukushima since 2002, we have gained experience of over 20 years by delivering certified Japanese vehicles globally. Our vision still remains the same; simplify car purchasing for all customers, regardless of geographical limitations. </p>
+                </div>
+              </div>
+
+              <div class="row g-3">
+                <div class="col-md-6 col-sm-12 order-md-1 order-sm-2 order-2  d-flex flex-column justify-content-center">
+                  <h2 class="why-title w-100">23 International Offices For Regional Services</h2>
+                  <p class="why-title-para">We have a widespread network of local offices in several different regions of all continents. Our local offices and agents aid the flow of import, they ensure that customs and shipment processes for vehicle delivery run smoothly. </p>
+                </div>
+                <div class="col-md-6 col-sm-12 order-md-2 order-sm-1 order-1 text-end img-div">
+                  <img src="{{asset('assets/images/staticPagesAssets/images/whysell-5.webp')}}" width="500" height="331" class="sellimage img-fluid" loading="lazy" alt="sat img">
+                </div>
+              </div>
+
+
+              <div class="row g-3">
+                <div class="col-md-6 col-sm-12 text-start img-div">
+                  <img src="{{asset('assets/images/staticPagesAssets/images/whysell-3.webp')}}" width="500" height="331" class="sellimage img-fluid" loading="lazy" alt="sat img">
+                </div>
+                <div class="col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-end">
+                  <h2 class="why-title w-100 text-left">100,000+ Site Visits Daily</h2>
+                  <p class="why-title-para">Being the leading car exporting site globally, we have established a loyal car enthusiast community, with thousands of new members joining every day. Users have the opportunity to explore our vast directory, compare prices, and use live chat support, which is all designed for seamless surfing.</p>
+                </div>
+              </div>
+
+              <div class="row g-3 mb-5">
+                <div class="col-md-6 col-sm-12 order-md-1 order-sm-2 order-2 d-flex flex-column justify-content-center">
+                  <h2 class="why-title w-100">Attracting Millions Of Car Buyers</h2>
+                  <p class="why-title-para">Over the years we have invested in our online platform and marketing advertisements to reach the maximum number of consumers. This has assisted SAT Japan in gaining widespread dominance of the market. </p>
+                </div>
+                <div class="col-md-6  col-sm-12 order-md-2 order-sm-1 order-1 text-end img-div">
+                  <img src="{{asset('assets/images/staticPagesAssets/images/whysell-4.webp')}}" width="500" height="331" class="sellimage img-fluid" loading="lazy" alt="sat img">
+                </div>
+              </div>
+        </div>
+    </div>
+</section>
