6be19a5c1915862df15aa768189f49a273794c04
MuhammadHammadArshad123 <hammadarshad@satjapan.co>
2025-01-24 18:57:13 +0500

    bugs fix testing

--- resources/views/static_pages/partial/carrier-service.blade.php
+++ resources/views/static_pages/partial/carrier-service.blade.php
@@ -1,9 +1,126 @@
-<section class="carrier_service_policy_desktop">
+<section class="carrier_service_policy_desktop desktop2-view">
     <div class="carrierService">
         <div class="container">
             <div class="row">
                 <div class="bannerImage">
                     <img src="{{asset('assets/images/staticPagesAssets/images/car-carrier.webp')}}" alt="Car Carrier" class="img-fluid" loading="lazy">
+                </div>
+
+                <div class="innerDetails">
+                    <p class="m-0 heading pt-4 pb-2">Detail</p>
+                    <p class="m-0 para">At SAT, we are devoted to continuously offering efficient and trouble-free car delivery through our car carrier service. 
+                    Whether you require special care for your vehicle or an economic solution for transportation, our company will offer you the most suitable option. 
+                    Through our car carrier service, you are guaranteed that your automobile will be transported effectively and safely to your preferred destination.</p>
+
+                    <p class="m-0 heading pt-4 pb-3">Delivery Options</p>
+
+                   <div class="deliveryOption d-flex justify-content-between align-items-center gap-3">
+                    <div class="col-md-6">
+                        <div class="left my-2">
+                            <div class="topImg">
+                                <img src="{{asset('assets/images/staticPagesAssets/images/carrier-service-body-img1.webp')}}" alt="Car Carrier" class="img-fluid w-100" loading="lazy">
+                            </div>
+
+                            <div class="title">
+                                <p class="m-0 pt-4 pb-3">Single Car Trailer</p>
+                            </div>
+
+                            <div class="detail">
+                                <p class="m-0">
+                            Your vehicle is shipped individually and undergoes careful handling during the transport
+                            process to your desired location. This service is ideal for high-value or special vehicles
+                            that have to be handled with extra care.
+                            </p>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="col-md-6">
+                        <div class="right my-2">
+                            <div class="topImg">
+                                <img src="{{asset('assets/images/staticPagesAssets/images/carrier-service-body-img2.webp')}}" alt="Car Carrier" class="img-fluid w-100" loading="lazy">
+                            </div>
+
+                            <div class="title">
+                                <p class="m-0 pt-4 pb-3 mt-1">Car Carrier Service</p>
+                            </div>
+
+                            <div class="detail">
+                                <p class="m-0">
+                                    Get your vehicle transported to your destination alongside other cars. 
+                                    This method allows multiple vehicles to be transported simultaneously, 
+                                    making it perfect for customers who want an affordable yet a high quality 
+                                    service.
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                   </div>
+
+                   <div class="carrierService">
+                    <p class="m-0 title pt-5 pb-2">Benefits of Our Carrier Service</p>
+
+                    <div class="inner row d-flex">
+                        <div class="col-lg-3 col-md-3 col-sm-6 py-2">
+                            <p class="m-0 maxSec">Maximum Security</p>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6 py-2">
+                            <p class="m-0 maxSec">Peace of Mind</p>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6 py-2">
+                            <p class="m-0 maxSec">Timely Delivery</p>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-6 py-2">
+                            <p class="m-0 maxSec">Dedicated Attention</p>
+                        </div>
+                    </div>
+
+                   </div>
+
+                   <div class="utilizeCar">
+                    <p class="m-0 title pt-5 pb-3">Steps to Utilize Our Car Carrier Service</p>
+
+                    <div class="inner row flex-lg-wrap justify-content-center mb-5 pb-5">
+                        <div class="col-lg-4 col-md-6 col-sm-6 py-2 mb-5">
+                            <div class="inner border rounded-2 text-center p-4">
+                                <img src="{{asset('assets/images/staticPagesAssets/icon/Ship-icon.webp')}}" alt="Ship" class="img-fluid" loading="lazy">
+                                <p class="m-0 head py-3">1. Select Delivery Option</p>
+                                <p class="m-0 para text-center">When placing your order, select either the single-car trailer or the car carrier service.</p>
+                            </div> 
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 py-2 mb-5">
+                            <div class="inner border rounded-2 text-center p-4">
+                                <img src="{{asset('assets/images/staticPagesAssets/icon/finance-expert.webp')}}" alt="Ship" class="img-fluid" loading="lazy">
+                                <p class="m-0 head py-3">2. Connect With Sales Team</p>
+                                <p class="m-0 para text-center">Consult our sales team to get details on how your vehicle will get transported.</p>
+                            </div> 
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-6 py-2 mb-5">
+                            <div class="inner border rounded-2 text-center p-4">
+                                <img src="{{asset('assets/images/staticPagesAssets/icon/finance-ride-card.webp')}}" alt="Ship" class="img-fluid" loading="lazy">
+                                <p class="m-0 head py-3">3. Receive Your Vehicle</p>
+                                <p class="m-0 para text-center">Receive your car at your doorstep and enjoy your dream ride.</p>
+                            </div> 
+                        </div>
+                    </div>
+
+                   </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="carrier_service_policy_desktop mobile2-view">
+    <div class="carrierService">
+        <div class="container">
+            <div class="row">
+                <div class="bannerImage">
+                    <img src="{{asset('assets/images/staticPagesAssets/images/car-carrier-mob.webp')}}" alt="Car Carrier" class="img-fluid" loading="lazy">
                 </div>
 
                 <div class="innerDetails">
@@ -113,4 +230,3 @@
         </div>
     </div>
 </section>
-
--- resources/views/static_pages/partial/finance_service.blade.php
+++ resources/views/static_pages/partial/finance_service.blade.php
@@ -1,4 +1,4 @@
-<section class="finance_service_desktop">
+<section class="finance_service_desktop desktop2-view">
     <div class="financeService">
         <div class="container">
             <div class="row">
@@ -81,7 +81,140 @@
 
                     <div class="ourFinanceService">
                         <p class="m-0 heading py-3">Why Choose Our Finance Service</p>
-                        <div class="row financ-emore-cookies flex-lg-wrap">
+                        <div class="row financ-emore-cookies flex-lg-wrap mb-5 pb-5">
+                            <div class="col-lg-3 col-md-6 col-sm-6 finance-services-card">
+                                <div class="card finance-services-card-work">
+                                    <img src="{{asset('assets/images/staticPagesAssets/icon/finance-bitcoin.webp')}}" class="why-chose-finance align-self-center" alt="">
+                                    <div class="finance-body-inner">
+                                        <h2 class="finance-services-hdr">Financial Flexibility</h2>
+                                        <p class="finance-services-para">Pay in installments to reduce financial strain.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-sm-6 finance-services-card">
+                                <div class="card finance-services-card-work">
+                                    <img src="{{asset('assets/images/staticPagesAssets/icon/finance-ownership.webp')}}" class="why-chose-finance align-self-center" alt="">
+                                    <div class="finance-body-inner">
+                                        <h2 class="finance-services-hdr">Immediate Ownership</h2>
+                                        <p class="finance-services-para">Take the car home without any further delay.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-sm-6 finance-services-card">
+                                <div class="card finance-services-card-work">
+                                    <img src="{{asset('assets/images/staticPagesAssets/icon/finance-better-modal.webp')}}" class="why-chose-finance align-self-center" alt="">
+                                    <div class="finance-body-inner">
+                                        <h2 class="finance-services-hdr">Access Better Models</h2>
+                                        <p class="finance-services-para">Pay for higher-end models in convenient installments.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 col-sm-6 finance-services-card">
+                                <div class="card finance-services-card-work">
+                                    <img src="{{asset('assets/images/staticPagesAssets/icon/finance-expert.webp')}}" class="why-chose-finance align-self-center" alt="">
+                                    <div class="finance-body-inner">
+                                        <h2 class="finance-services-hdr">Expert Assistance</h2>
+                                        <p class="finance-services-para">Get premium support from our dedicated staff.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="finance_service_desktop mobile2-view">
+    <div class="financeService">
+        <div class="container">
+            <div class="row">
+                <div class="bannerImg">
+                    <img src="{{asset('assets/images/staticPagesAssets/images/finance-service-mob.webp')}}" alt="Banner" class="img-fluid" loading="lazy">
+                </div>
+
+                <div class="innerDetail">
+                    <p class="m-0 heading pt-4 pb-2">Detail</p>
+                    <p class="m-0 para">We understand that buying a car is not a small investment, and hence, customers
+                        should be offered convenient modes of payment. Our finance service offers payment flexibility to our
+                        customers, where they can pay for their order with ease.
+                    </p>
+
+                    <div class="payInstallment d-flex justify-content-between align-items-center my-5">
+                        <div class="centeralized">
+                            <div class="htb-heading">
+                                <h1 class="title ms-5 ps-3">Pay With Easy Installments</h1>
+                                <h2 class="para ms-5 ps-3">Go through this video to learn more about our finance service.</h2>
+                            </div>
+                        </div>
+                        <div class="sat-video-frame">
+                            <iframe class="brd-8x" width="502" height="272" src="https://www.youtube.com/embed/38vGOQlTIE8?si=CftmKfprm9qc7oRp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+                        </div>
+                    </div>
+
+                    <div class="row finance-services-custom flex-lg-wrap">
+                        <div class="col-lg-3 col-md-6 col-sm-6 finance-services-card">
+                            <div class="card finance-services-card-work">
+                                <img src="{{asset('assets/images/staticPagesAssets/icon/finance-slect-vehicle.webp')}}" class="why-chose-finance align-self-center" alt="">
+                                <div class="finance-body-inner">
+                                    <p class="finance-services-hdr">Select Your Vehicle</p>
+                                    <p class="finance-services-para">Check out our wide inventory of Japanese used cars and select the model that meets your needs.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 finance-services-card">
+                            <div class="card finance-services-card-work">
+                                <img src="{{asset('assets/images/staticPagesAssets/icon/finance-sale-department.webp')}}" class="why-chose-finance align-self-center" alt="">
+                                <div class="finance-body-inner">
+                                    <p class="finance-services-hdr">Contact Sales</p>
+                                    <p class="finance-services-para">Contact our sales department to discuss details of the most suitable monthly payment plan for you.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 finance-services-card">
+                            <div class="card finance-services-card-work">
+                                <img src="{{asset('assets/images/staticPagesAssets/icon/finance-investment-selection.webp')}}" class="why-chose-finance align-self-center" alt="">
+                                <div class="finance-body-inner">
+                                    <p class="finance-services-hdr">Finalize Payment Method</p>
+                                    <p class="finance-services-para">After finalizing the plan, pay with one of our payment methods, such as Credit/Debit card, PayPal, or Bank Transfer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 finance-services-card">
+                            <div class="card finance-services-card-work">
+                                <img src="{{asset('assets/images/staticPagesAssets/icon/finance-ride-card.webp')}}" class="why-chose-finance align-self-center" alt="">
+                                <div class="finance-body-inner">
+                                    <p class="finance-services-hdr">Enjoy Your Ride</p>
+                                    <p class="finance-services-para">
+                                        Drive your dream car as soon as it is delivered and pay off the
+                                        remaining balance in installments.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <p class="m-0 heading pt-5 pb-3">Flexible Payment Plans</p>
+                    <p class="m-0 para">
+                        We offer affordable payment plans, enabling you to make monthly installments for 24 months to clear your car. 
+                        This helps you use your new car straight away while preventing the pressure of a large initial payment. When you decide to go 
+                        for SAT’s finance service, you can make the purchase and pay later in equal flexible installments and get the car you desire.
+                    </p>
+
+                    <div class="bannerImg my-4">
+                        <img src="{{asset('assets/images/staticPagesAssets/images/finance-body-img.webp')}}" alt="Banner" class="img-fluid" loading="lazy">
+                    </div>
+
+                    <div class="ourFinanceService">
+                        <p class="m-0 heading py-3">Why Choose Our Finance Service</p>
+                        <div class="row financ-emore-cookies flex-lg-wrap mb-5 pb-5">
                             <div class="col-lg-3 col-md-6 col-sm-6 finance-services-card">
                                 <div class="card finance-services-card-work">
                                     <img src="{{asset('assets/images/staticPagesAssets/icon/finance-bitcoin.webp')}}" class="why-chose-finance align-self-center" alt="">
