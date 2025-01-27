a2e560240ffa7c9cb1d4357c677783bc38f4a4c5
MuhammadHammadArshad123 <hammadarshad@satjapan.co>
2025-01-27 12:10:12 +0500

    bugs test

--- resources/views/static_pages/partial/stolen.blade.php
+++ resources/views/static_pages/partial/stolen.blade.php
@@ -1,9 +1,9 @@
-<section class="non_stolen_vehicle_desktop">
+<section class="non_stolen_vehicle_desktop desktop2-view">
     <div class="nonStolenVehicle">
         <div class="container">
             <div class="row">
                 <div class="bannerImage">
-                    <img src="{{asset('assets/images/staticPagesAssets/images/non-stolen-vehicle-check-service.webp')}}" alt="Banner" class="img-fluid" loading="lazy">
+                    <img src="{{asset('assets/images/staticPagesAssets/images/Nonstolen.webp')}}" alt="Banner" class="img-fluid" loading="lazy">
                 </div>
 
                 <div class="innerDetails">
@@ -24,6 +24,54 @@
                             <li class="my-4">Fill in the details in the pop-up that comes and make a payment of 30,000 YEN. Alternatively, you can send an email to info@satjapan.com and our support team will reach out to you and guide you about the next steps.</li>
                             <li class="my-4">After your details have been submitted, the verification through relevant agencies will take 5-8 working days.</li>
                             <li class="my-4">Soon after, your verification will be dispatched to your local address through Japan's postal code.</li>
+                        </ol>
+                    </div>
+
+                    <button class="btn btn-verified">Begin Verification</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="helpNeed mt-4">
+            <div class="container">
+                <div class="row">
+                    <p class="m-0 head pt-4 pb-2">Need Help?</p>
+                    <p class="m-0 tail pb-5">In case of any questions, please feel free to reach out to us at <a href="javascript:;">info@satjapan.com</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+<section class="non_stolen_vehicle_desktop mobile2-view">
+    <div class="nonStolenVehicle">
+        <div class="container">
+            <div class="row">
+                <div class="bannerImage">
+                    <img src="{{asset('assets/images/staticPagesAssets/images/Nonstolen.webp')}}" alt="Banner" class="img-fluid" loading="lazy">
+                </div>
+
+                <div class="innerDetails">
+                    <p class="m-0 heading pt-4 pb-3">Detail</p>
+                    <p class="m-0 para">
+                        The cases of vehicle theft in Japan are unfortunately common; every year thousands of cars, buses, motorbikes, vans, etc. 
+                        are stolen and exported to foreign countries. To avoid such mishaps, we offer our customers an advanced stolen car checking 
+                        procedure to confirm whether the vehicle has been stolen or not. If you are purchasing a vehicle through SAT and are in doubt 
+                        about the vehicle's history, you can request an inspection certificate to confirm the status of your vehicle.
+                    </p>
+
+                    <img src="{{asset('assets/images/staticPagesAssets/images/stolen-bannar2-mob.webp')}}" alt="Banner" class="img-fluid py-3" loading="lazy">
+
+                    <p class="m-0 heading pb-3">Steps to get a Non-Stolen Verification Certificate</p>
+                    <div class="list">
+                        <ol>
+                            <li class="my-2">Start by clicking on the 'Begin Verification' button below.</li>
+                            <li class="my-2">Fill in the details in the pop-up that comes and make a payment of 30,000 YEN. Alternatively, you can send an email to info@satjapan.com and our support team will reach out to you and guide you about the next steps.</li>
+                            <li class="my-2">After your details have been submitted, the verification through relevant agencies will take 5-8 working days.</li>
+                            <li class="my-2">Soon after, your verification will be dispatched to your local address through Japan's postal code.</li>
                         </ol>
                     </div>
 
